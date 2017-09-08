a0514a4a306746e1dc8b27eb904b23a604e62a41
Ukhin Mikhail <mikhail.ukhin@gmail.com>
2017-09-08 23:25:31 +0300

    upd

---
+++ WebApplication6/Controllers/MoviesController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication6.Models;
+using WebApplication6.ViewModels;
+
+namespace WebApplication6.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie { Name = "Shrek!" };
+
+            //return Content("Hi dude");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new {page = 1, sortBy = "name"});
+            //return JavaScript("<script>alert(1)</script>");
+
+            //ViewData["Movie"] = movie;
+            //ViewBag.Movie = movie;
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Bobby"},
+                new Customer {Name = "Cristy"},
+                new Customer {Name = "Robby"},
+                new Customer {Name = "Mattew"},
+            };
+;
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (string.IsNullOrEmpty(sortBy))
+                sortBy = "Name";
+
+            return Content($"pageIndex={pageIndex}&sortBy={sortBy}");
+        }
+
+        [Route("movies/released/{year:int}/{month:regex(\\d{1}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+       
+    }
+}
---
+++ WebApplication6/ViewModels/RandomMovieViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication6.Models;
+
+namespace WebApplication6.ViewModels
+{
+    public class RandomMovieViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
