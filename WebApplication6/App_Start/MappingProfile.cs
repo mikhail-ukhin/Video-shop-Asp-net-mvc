742cc0cefe11a7712d1fc0b5f37f692db3466efb
Ukhin Mikhail <mikhail.ukhin@gmail.com>
2017-09-13 21:25:42 +0300

    added web api + mapper

---
+++ WebApplication6/App_Start/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using WebApplication6.Dtos;
+using WebApplication6.Models;
+
+namespace WebApplication6.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Customer, CustomerDto>();
+            CreateMap<CustomerDto, Customer>();
+        }
+    }
+}
